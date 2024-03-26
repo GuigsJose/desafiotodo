10c3f872b7ddf52306eef05ecf406add50fd9f19
Guilherme Silva <guizaomtz@gmail.com>
2024-03-26 11:04:40 -0300

    Criação da entidade do desafio na camada Entity

---
+++ desafio-todolist/src/main/java/br/com/guilhermesilva/desafiotodolist/entity/Todo.java
@@ -0,0 +1,29 @@
+package br.com.guilhermesilva.desafiotodolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+
+}
